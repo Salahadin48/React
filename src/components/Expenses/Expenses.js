b57423fcd18f0fd4e15cd367cd420113c82036cc
Salahadin48 <salahadin48@outlook.com>
2022-03-26 12:41:32 -0500

    Wrapper component added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,13 +1,15 @@
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import Card from "../UI/Card";
+import React from 'react';
 
 function Expenses(props) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       {props.items.map((e) => (
         <ExpenseItem title={e.title} amount={e.amount} date={e.date} />
       ))}
-    </div>
+    </Card>
   );
 }
 
